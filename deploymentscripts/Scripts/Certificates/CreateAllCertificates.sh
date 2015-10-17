815f66084a10e955ca4e691d074e83c54bea4114
Bruno Braga <bruno.mh.braga@gmail.com>
2015-10-17 08:57:56 -0700

    Ready to start TSS server.

--- deploymentscripts/Scripts/Certificates/CreateAllCertificates.sh
+++ deploymentscripts/Scripts/Certificates/CreateAllCertificates.sh
@@ -29,3 +29,4 @@ DeveloperDir="Developer"
 "./$GenCertificateScript" "$DeveloperDir" "$MonitorDir"
 
 
+
--- deploymentscripts/Scripts/PrepareEnvAndRunPCop.sh
+++ deploymentscripts/Scripts/PrepareEnvAndRunPCop.sh
@@ -1,18 +1,28 @@
 sudo yum -y update
 sudo yum -y install java-1.8.0-openjdk-src.x86_64 && sudo yum -y install java-1.8.0-openjdk-headless.x86_64  && sudo yum -y install java-1.8.0-openjdk-devel.x86_64
+sudo yum -y install tpm-tools trousers jsvc
 
 UploadedFilesDir="UploadedFiles/"
+JTSS_FILES_DIR="$UploadedFilesDir""jTSS_0.7.1a/"
 SrcDir="$UploadedFilesDir""src/"
 BinDir="$UploadedFilesDir""bin/"
 CertificatesDir="$UploadedFilesDir""Certificates/"
 CreateStoresScript="CreateStores.sh"
 
 
+sudo modprobe tpm
+sudo modprobe tpm_bios tpm_infineon tpm_nsc tpm_tis
+#service tcsd start
+
 #Certficates:
 #1.Run createallsertificates.sh
 #2.go to each directory and run the createstores.sh
 
 hostFolder=""
+#cd "$JTSS_FILES_DIR""soap"
+cd "$JTSS_FILES_DIR""soap"
+sudo bash -c "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk && ./tcs_daemon.sh start"
+cd
 if [ "$1" == "-M" ]
 then
 	echo "On monitor..."
